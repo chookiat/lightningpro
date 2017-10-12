01a45c91e7f2969d8cf0d30da8005c531add0f68
chookiat <chookiat.s@sahapat.co.th>
2017-10-12 13:20:38 +0700

    new project my_library django

---
+++ my_library/book_management/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    publish_date = models.DateField('date pulished')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
---
+++ my_library/book_management/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+#from django.http import HttpResponse
+#from django.template import loader
+from book_management.models import Category, Book
+
+# Create your views here.
+def index(request):
+    #header_str = 'Hello, Python variable'
+    #template = loader.get_template('index.html')
+    #context = {
+    #    'val1': header_str
+    #}
+    context = {
+        'cat_num': len(Category.objects.all()),
+        'book_num': len(Book.objects.all()) 
+    }
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'index.html', context)
